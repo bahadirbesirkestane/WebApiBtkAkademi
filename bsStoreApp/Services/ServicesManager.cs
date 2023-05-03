d115763d41a45df2b328690430556a26f0d78a74
isim soyisim <bahadir_kestane@hotmail.com>
2023-05-03 18:13:03 +0300

    Manager işlemleri Katmanlı mimari

---
+++ bsStoreApp/Services/ServicesManager.cs
@@ -0,0 +1,21 @@
+﻿using Repositories.Contracts;
+using Services.Contrats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ServicesManager : IServicesManager
+    {
+        private readonly Lazy<IBookServices> _bookServices;
+        public ServicesManager(IRepositoryManager repositoryManager)
+        {
+            _bookServices= new Lazy<IBookServices>(() =>new BookManager(repositoryManager));
+        }
+
+        public IBookServices BookServices => _bookServices.Value;
+    }
+}
