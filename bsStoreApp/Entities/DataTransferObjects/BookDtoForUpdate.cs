daf01b25aaf1875f8e97e2bf2b2674b8f3c28895
isim soyisim <bahadir_kestane@hotmail.com>
2023-05-07 21:50:04 +0300

    Data transfer altında bir "dto" ekledik

---
+++ bsStoreApp/Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    //record -- classlar açok benzer ve struct-
+    //-gibi referans tiplidir
+    public record BookDtoForUpdate(int Id,string Title,decimal Price);
+
+    // veya bu alttaki blok gibi de olur
+    // burada init değilde hala set olarak kalırsa readonly olmaz
+    // biz readonly olmasını istiyoruz 
+    // bir kere atanacak bidaha değiştirilemeyecek.
+
+    //public record BookDtoForUpdate
+    //{
+    //    public int Id { get; init; }
+    //    public string Title { get; init; }
+    //    public decimal Price { get; init; }
+    //}
+
+    
+}
